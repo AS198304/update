569134e879650fd073a601e47e7a250c1cffb8f2
Pigeon Telecom <noc@pigeontele.com>
2024-01-26 09:04:58 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 设置下载链接
+download_url="https://codeload.github.com/MoeDove-LLC/Bird2-Config/zip/refs/tags/v1.5.0?token=BDJNIQAO4ML3TJINHS7NZVDFWMCL4"
+
+# 设置本地解压目录
+extract_dir="Bird2-Config-1.5.0"
+
+# 设置目标目录
+target_dir="/etc/bird"
+
+# 下载文件
+wget $download_url -O bird2-config.zip
+
+# 解压文件
+unzip bird2-config.zip -d $extract_dir
+
+# 复制文件到目标目录
+cp -r $extract_dir/Bird2-Config-1.5.0/node/functions $target_dir
+cp -r $extract_dir/Bird2-Config-1.5.0/node/tools $target_dir
+cp $extract_dir/Bird2-Config-1.5.0/node/version.txt $target_dir
+cp $extract_dir/Bird2-Config-1.5.0/node/bird.conf $target_dir
+
+birdc c
+
+# 清理临时文件
+rm -rf bird2-config.zip $extract_dir
+
+echo "脚本执行完成。"
